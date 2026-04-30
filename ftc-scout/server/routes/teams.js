21c44264c335ed141dcb81154e6104eddd145b32
Guilleeeeeeeee <guillecapo34@gmail.com>
2026-04-30 17:47:05 -0300

    update teams.js

--- ftc-scout/server/routes/teams.js
+++ ftc-scout/server/routes/teams.js
@@ -110,7 +110,7 @@ router.get('/:number', validateTeamNumber, async (req, res) => {
     res.json(team);
   } catch (err) {
     console.error('[teams/:number]', err.message);
-    res.status(502).json({ error: 'Failed to fetch team data.' });
+    res.status(null);
   }
 });
 
